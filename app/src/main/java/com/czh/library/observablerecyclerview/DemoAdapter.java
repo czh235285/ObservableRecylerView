a5ce4a409a94fbc2f1e8b8783b7f710467a26171
chenzhihui <5741034@qq.com>
2018-04-16 14:45:50 +0800

    first commit

---
+++ app/src/main/java/com/czh/library/observablerecyclerview/DemoAdapter.java
@@ -0,0 +1,24 @@
+package com.czh.library.observablerecyclerview;
+
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+import java.util.List;
+
+/**
+ * Created by admin on 2018/4/16.
+ */
+
+public class DemoAdapter extends BaseQuickAdapter<String,BaseViewHolder>{
+    public DemoAdapter(@LayoutRes int layoutResId, @Nullable List<String> data) {
+        super(layoutResId, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, String item) {
+helper.setText(R.id.text, item);
+    }
+}
---
+++ app/src/main/java/com/czh/library/observablerecyclerview/MainActivity.java
@@ -0,0 +1,57 @@
+package com.czh.library.observablerecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+import android.widget.TextView;
+
+import com.czh.library.library.ObservableRecylerView;
+import com.czh.library.library.ObservableScrollViewCallbacks;
+import com.czh.library.library.ScrollState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ObservableScrollViewCallbacks {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+        ObservableRecylerView rcv = (ObservableRecylerView) findViewById(R.id.rcv);
+        rcv.setScrollViewCallbacks(this);
+        List<String> a = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            a.add(i + "");
+        }
+        rcv.setLayoutManager(new LinearLayoutManager(this));
+        rcv.setAdapter(new DemoAdapter(R.layout.item, a));
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+        if (scrollState == ScrollState.UP) {
+            if (tv.getVisibility() == View.VISIBLE) {
+                tv.setVisibility(View.GONE);
+            }
+        } else if (scrollState == ScrollState.DOWN) {
+            if (tv.getVisibility() == View.GONE) {
+                tv.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+}
